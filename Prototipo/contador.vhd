5abbb34ed834b2c0b819bdac5b2345f5d4835cca
Joao-vitor1 <50772495+Joao-vitor1@users.noreply.github.com>
2024-11-02 03:10:03 -0300

    Add files via upload

--- Prototipo/contador.vhd
+++ Prototipo/contador.vhd
@@ -12,43 +12,45 @@ architecture logica of contador is
                           q : out std_logic);
 	end component;
 	
-	signal cin: std_logic_vector(7 downto 0);
+	signal cin, valor_saida: std_logic_vector(9 downto 0);
 begin
 	ffjk1: ffjk
-		port map(clk, clear, '1', '1','1', cout(0));
+		port map(clk, clear, '1', '1','1', valor_saida(0));
 
 	ffjk2: ffjk
-		port map(clk, clear, '1', cout(0),cout(0), cout(1));
+		port map(clk, clear, '1', valor_saida(0),valor_saida(0), valor_saida(1));
 	
-	cin(0) <= cout(0) and cout(1);
+	cin(0) <= valor_saida(0) and valor_saida(1);
 	ffjk3: ffjk
-		port map(clk, clear, '1', cin(0),cin(0), cout(2));
+		port map(clk, clear, '1', cin(0),cin(0), valor_saida(2));
 	
-	cin(1) <= cout(1) and cout(2);
+	cin(1) <= valor_saida(1) and valor_saida(2);
 	ffjk4: ffjk
-		port map(clk, clear, '1', cin(1),cin(1), cout(3));
+		port map(clk, clear, '1', cin(1),cin(1), valor_saida(3));
 	
-	cin(2) <= cout(2) and cout(3);
+	cin(2) <= valor_saida(2) and valor_saida(3);
 	ffjk5: ffjk
-		port map(clk, clear, '1', cin(2),cin(2), cout(4));
+		port map(clk, clear, '1', cin(2),cin(2), valor_saida(4));
 	
-	cin(3) <= cout(3) and cout(4);
+	cin(3) <= valor_saida(3) and valor_saida(4);
 	ffjk6: ffjk
-		port map(clk, clear, '1', cin(3),cin(3), cout(5));
+		port map(clk, clear, '1', cin(3),cin(3), valor_saida(5));
 		
-	cin(4) <= cout(4) and cout(5);
+	cin(4) <= valor_saida(4) and valor_saida(5);
 	ffjk7: ffjk
-		port map(clk, clear, '1', cin(4),cin(4), cout(6));
+		port map(clk, clear, '1', cin(4),cin(4), valor_saida(6));
 	
-	cin(5) <= cout(5) and cout(6);
+	cin(5) <= valor_saida(5) and valor_saida(6);
 	ffjk8: ffjk
-		port map(clk, clear, '1', cin(5),cin(5), cout(7));
+		port map(clk, clear, '1', cin(5),cin(5), valor_saida(7));
 	
-	cin(6) <= cout(6) and cout(7);
+	cin(6) <= valor_saida(6) and valor_saida(7);
 	ffjk9: ffjk
-		port map(clk, clear, '1', cin(6),cin(6), cout(8));
+		port map(clk, clear, '1', cin(6),cin(6), valor_saida(8));
 	
-	cin(7) <= cout(7) and cout(8);
+	cin(7) <= valor_saida(7) and valor_saida(8);
 	ffjk10: ffjk
-		port map(clk, clear, '1', cin(7),cin(7), cout(9));
+		port map(clk, clear, '1', cin(7),cin(7), valor_saida(9));
+	
+	cout <= valor_saida;
 end logica;
